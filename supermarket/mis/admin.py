e915c17140b42198912c3705c168935c9660a079
jakobwamani <nicobwan@gmail.com>
2021-01-09 12:39:46 +0300

    SetUpDevEnv

--- supermarket/mis/admin.py
+++ supermarket/mis/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from mis.models import stock
+
+admin.site.register(stock)
--- supermarket/mis/models.py
+++ supermarket/mis/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class stock(models.Model):
+    product_name = models.CharField(max_length=200)
+    quantity = models.IntegerField(default=0)
+    cost_price = models.IntegerField(default=0)
+    category = models.CharField(max_length=200)
+    selling_price = models.IntegerField(default=0)
+    pub_date = models.DateTimeField('date published')
+    
